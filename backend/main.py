f3e98ed1043b64dbdc8533f62c42968f21aae3ef
Aayan Mapara <amapara2007@gmail.com>
2025-12-15 17:05:07 -0700

    Returns historical prices

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
 import requests
 import pandas
+import time
 import os
 
 from dotenv import load_dotenv
 load_dotenv()
+
+from collections import defaultdict
 
 COINS = {
     "BTC" : "bitcoin", 
@@ -20,7 +23,7 @@ COINS = {
 
 COIN_GECKO_API_KEY = os.getenv("COIN_GECKO_API_KEY")
 
-def get_prices(symbols):
+def get_curr_prices(symbols):
     prices = {}
 
     for symbol in symbols:
@@ -40,16 +43,45 @@ def get_prices(symbols):
         
     return prices
 
+def get_historical_prices(symbols):
+    prices = defaultdict(list)
+
+    for symbol in symbols:
+        id = COINS[symbol]
+        url = f"https://api.coingecko.com/api/v3/coins/{id}/market_chart?vs_currency=usd&days=90&x_cg_demo_api_key={COIN_GECKO_API_KEY}"
+
+        try:
+            data = requests.get(url)
+            info = data.json()
+
+            coin_prices = info["prices"]
+            prices[symbol].append(coin_prices)
+
+        except Exception as e:
+            print(f"Error finding prices : {e}")
+
+        time.sleep(1)
+    
+    return prices
+
+
 def main():
     symbols = []
 
-    print("Enter your top 5 most held coins:")
+    num = int(input("Enter amount of coins held: "))
 
-    for i in range(5):
+    print(f"Enter your top {num} most held coins:")
+
+    for i in range(num):
         symbol = input()
         symbols.append(symbol)
+    
+    res = get_historical_prices(symbols)
 
-    print(get_prices(symbols))
+    print(res)
+
+    for key in res:
+        print(key)
 
 if __name__ == "__main__":
     main()
